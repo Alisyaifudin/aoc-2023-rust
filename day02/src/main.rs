c1983b8efa97cbb5e0557591cd78fe3a0a508ec3
alisyaifudin <muhammad.ali.syaifudin@hotmail.com>
2023-12-14 15:25:59 +0800

    day2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,23 +1,35 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::num::ParseIntError;
 use std::path::PathBuf;
 
 #[derive(Debug)]
-enum Error {
-    NoArg,
-    InvalidArg,
-    Io(io::Error),
+enum Invalid<'a> {
+    Arg(&'a str),
+    Line(&'a str),
+    Cube(&'a str),
+    Color(&'a str),
+    Parse(ParseIntError),
+    Id(&'a str),
 }
 
-fn main() -> Result<(), Error> {
+#[derive(Debug)]
+enum Error<'a> {
+    NoArg(&'a str),
+    Io(io::Error),
+    Inv(Invalid<'a>),
+}
+
+fn main() -> Result<(), Error<'static>> {
     let root_dir = env::current_dir().expect("Failed to get current working directory");
     let args: Vec<String> = env::args().collect();
-
+ 
     // to test with sample data
-    // let file_path = root_dir.join("src").join("sample-2.txt");
+    // let file_path = root_dir.join("src").join("sample-1.txt");
     let file_path = root_dir.join("src").join("input.txt");
-    
+
     // Create a Line instance from a file
     let mut line_reader = Line::new(file_path).map_err(Error::Io)?;
 
@@ -27,20 +39,16 @@ fn main() -> Result<(), Error> {
             "1" => part_one,
             "2" => part_two,
             _ => {
-                eprintln!("Invalid argument. Use '1' or '2'");
-                return Err(Error::InvalidArg);
+                return Err(Error::Inv(Invalid::Arg("Invalid argument. Use '1' or '2'")));
             }
         },
         None => {
-            eprintln!("Missing argument. Use '1' or '2'");
-            return Err(Error::NoArg);
+            return Err(Error::NoArg("Missing argument. Use '1' or '2'"));
         }
     };
     loop {
         match line_reader.read() {
-            Ok(Some(line)) => {
-                total += func(line);
-            }
+            Ok(Some(line)) => total += func(line).map_err(Error::Inv)?,
             Ok(None) => {
                 // End of file reached
                 break;
@@ -83,104 +91,150 @@ impl Line {
     }
 }
 
-fn char_to_digit(c: char) -> Option<u8> {
-    if c.is_digit(10) {
-        Some(c as u8 - b'0')
+#[derive(Clone)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+const RED: u16 = 12;
+const GREEN: u16 = 13;
+const BLUE: u16 = 14;
+
+impl Color {
+    fn is_invalid(&self, num: u16) -> bool {
+        match self {
+            Color::Red => num > RED,
+            Color::Green => num > GREEN,
+            Color::Blue => num > BLUE,
+        }
+    }
+}
+
+fn create_map() -> HashMap<&'static str, Color> {
+    let color_map: HashMap<&str, Color> = [
+        ("red", Color::Red),
+        ("green", Color::Green),
+        ("blue", Color::Blue),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    color_map
+}
+
+#[allow(dead_code)]
+fn part_one<'a>(line: String) -> Result<u32, Invalid<'a>> {
+    let splitted: Vec<&str> = line.split(":").collect();
+    let game_id = splitted
+        .get(0)
+        .map(|&s| {
+            s.split_ascii_whitespace()
+                .collect::<Vec<&str>>()
+                .get(1)
+                .map(|&id| id.parse::<u16>().map_err(Invalid::Parse))
+                .ok_or(Invalid::Id("No Id"))
+        })
+        .ok_or(Invalid::Id("No Game <Id>"))???;
+
+    let game = *splitted
+        .get(1)
+        .ok_or(Invalid::Line("The line does not contain ':'"))?;
+    let color_map = create_map();
+    let sets: Vec<&str> = game.split(";").collect();
+    let mut valid = true;
+
+    'outer: for set in sets {
+        let cubes: Vec<&str> = set.split(",").collect();
+        for cube in cubes {
+            let cube_info: Vec<&str> = cube.split_ascii_whitespace().collect();
+            let num = cube_info
+                .get(0)
+                .map(|&n| n.parse::<u16>().map_err(Invalid::Parse))
+                .ok_or(Invalid::Cube("No number found"))??;
+            let color = cube_info
+                .get(1)
+                .map(|&c| color_map.get(c).ok_or(Invalid::Color("Invalid color")))
+                .ok_or(Invalid::Cube("No color found"))??;
+            valid = !color.is_invalid(num);
+            if !valid {
+                break 'outer;
+            }
+        }
+    }
+    if valid {
+        Ok(game_id as u32)
     } else {
-        None
+        Ok(0)
+    }
+}
+
+#[derive(Debug)]
+struct Bag {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl Bag {
+    fn new() -> Self {
+        Bag {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+    fn update(&mut self, color: &Color, num: u32) {
+        match color {
+            Color::Blue => {
+                if self.blue < num {
+                    self.blue = num;
+                }
+            }
+            Color::Green => {
+                if self.green < num {
+                    self.green = num;
+                }
+            }
+            Color::Red => {
+                if self.red < num {
+                    self.red = num;
+                }
+            }
+        }
+    }
+    fn power(&self) -> u32 {
+        self.red * self.green * self.blue
     }
 }
 
 #[allow(dead_code)]
-fn part_one(line: String) -> u32 {
-    let mut digits: Vec<u8> = Vec::new();
-    for ch in line.chars() {
-        match char_to_digit(ch) {
-            Some(num) => digits.push(num),
-            None => (),
-        }
-    }
-    if digits.len() == 0 {
-        panic!("There must be number in each line! {}", line)
-    }
-    let first_digit = digits.first().unwrap();
-    let last_digit = digits.last().unwrap();
-    return (first_digit * 10 + last_digit) as u32;
-}
+fn part_two<'a>(line: String) -> Result<u32, Invalid<'a>> {
+    let splitted: Vec<&str> = line.split(":").collect();
 
-struct Digit {
-    digit: u8,
-    buff: Vec<char>,
-    spell_out: Vec<char>,
-}
-
-impl Digit {
-    fn new(digit: u8, spell_out: &str) -> Self {
-        Digit {
-            digit,
-            buff: Vec::new(),
-            spell_out: spell_out.chars().collect(),
+    let game = *splitted
+        .get(1)
+        .ok_or(Invalid::Line("The line does not contain ':'"))?;
+    let color_map = create_map();
+    let sets: Vec<&str> = game.split(";").collect();
+    let mut bag = Bag::new();
+    for set in sets {
+        let cubes: Vec<&str> = set.split(",").collect();
+        for cube in cubes {
+            let cube_info: Vec<&str> = cube.split_ascii_whitespace().collect();
+            let num = cube_info
+                .get(0)
+                .map(|&n| n.parse::<u16>().map_err(Invalid::Parse))
+                .ok_or(Invalid::Cube("No number found"))??;
+            let color = cube_info
+                .get(1)
+                .map(|&c| color_map.get(c).ok_or(Invalid::Color("Invalid color")))
+                .ok_or(Invalid::Cube("No color found"))??;
+            bag.update(color, num as u32);
         }
     }
-    fn update(&mut self, ch: char) -> Option<u8> {
-        let buff_len = self.buff.len();
-        if self.spell_out[buff_len] != ch {
-            if buff_len > 0 {
-                self.buff = Vec::new();
-            }
-            // start over
-            if self.spell_out[0] == ch {
-                self.buff.push(ch);
-            }
-            return None;
-        }
-        if self.spell_out.len() > buff_len + 1 {
-            self.buff.push(ch);
-            return None;
-        }
-        self.buff = Vec::new();
-        return Some(self.digit);
-    }
-}
-
-fn create_digits() -> Vec<Digit> {
-    let mut digits = Vec::new();
-    digits.push(Digit::new(0, "zero"));
-    digits.push(Digit::new(1, "one"));
-    digits.push(Digit::new(2, "two"));
-    digits.push(Digit::new(3, "three"));
-    digits.push(Digit::new(4, "four"));
-    digits.push(Digit::new(5, "five"));
-    digits.push(Digit::new(6, "six"));
-    digits.push(Digit::new(7, "seven"));
-    digits.push(Digit::new(8, "eight"));
-    digits.push(Digit::new(9, "nine"));
-    return digits;
-}
-
-#[allow(dead_code)]
-fn part_two(line: String) -> u32 {
-    let mut digits = Vec::new();
-    let mut spell_out_digits = create_digits();
-    for ch in line.chars() {
-        match char_to_digit(ch) {
-            Some(num) => {
-                digits.push(num);
-            }
-            None => (),
-        }
-        for dg in spell_out_digits.iter_mut() {
-            match dg.update(ch.to_ascii_lowercase()) {
-                Some(digit) => digits.push(digit),
-                None => (),
-            }
-        }
-    }
-    if digits.len() == 0 {
-        panic!("There must be number in each line! {}", line)
-    }
-    let first_digit = digits.first().unwrap();
-    let last_digit = digits.last().unwrap();
-    let result = (first_digit * 10 + last_digit) as u32;
-    return result;
+    // println!("{:?} = {}", bag, bag.power());
+    Ok(bag.power())
 }
